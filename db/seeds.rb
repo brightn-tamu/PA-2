8fd5f60132c3c53c3c090238df244c1a20e9a41d
Nathaniel Bright <brightn@tamu.edu>
2024-09-07 10:38:38 -0500

    Redo- P1-3

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,27 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+# Seed the RottenPotatoes DB with some movies.
+more_movies = [
+  {:title => 'My Neighbor Totoro', :rating => 'G',
+   :release_date => '16-Apr-1988'},
+  {:title => 'Green Book', :rating => 'PG-13',
+   :release_date => '16-Nov-2018'},
+  {:title => 'Parasite', :rating => 'R',
+   :release_date => '30-May-2019'},
+  {:title => 'Nomadland', :rating => 'R',
+   :release_date => '19-Feb-2021'},
+  {:title => 'CODA', :rating => 'PG-13',
+   :release_date => '13-Aug-2021'},
+  {:title => 'The Godfather', :rating => 'R',
+   :release_date => '24-Mar-1972' },
+  { :title => 'The Dark Knight', :rating => 'PG-13',
+    :release_date => '18-Jul-2008' },
+  { :title => 'Pulp Fiction', :rating => 'R',
+    :release_date => '14-Oct-1994' }
+]
+
+more_movies.each do |movie|
+  Movie.find_or_create_by!(movie)
+end
